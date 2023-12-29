6b3007d88dc5fecba28d6c382b1966003aec787c
alexandracuzea <gabriela.cuzea@student.upt.ro>
2023-12-29 12:00:35 +0200

    added job router and controller

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
-console.log("working...");
+import * as dotenv from "dotenv";
+const app = express();
+dotenv.config();
+import express from "express";
+import morgan from "morgan";
+
+//custom imports routers
+import jobRouter from "./routes/jobRouter.js";
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "data received", data: req.body });
+});
+
+app.use("/api/v1/jobs", jobRouter);
+
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: "something went wrong" });
+});
+
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log(`server sunning on port ${port}...`);
+});
